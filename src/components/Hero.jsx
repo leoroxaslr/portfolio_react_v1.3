7546830a2979ee3ee83b75c23ad31c2da03e64cb
leoroxaslr <leoroxas.lr@gmail.com>
2023-09-12 21:30:01 +0800

    fixed buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
 
             <a
               href="#"
-              className="inline-flex items-center justify-center ml-6  px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              className="inline-flex items-center justify-center ml-3  px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
               Projects
             </a>
